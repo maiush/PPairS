61ea1d0b0cc6445eadf578265670e2d00f9161bf
Sharan Maiya <lightbulbmoment22617@gmail.com>
2024-05-14 11:38:50 +0100

    update scraping script for science feedback

--- utils/science_feedback_scraping.py
+++ utils/science_feedback_scraping.py
@@ -1,43 +1,8 @@
-import time
 import requests
 from bs4 import BeautifulSoup
-
 import pandas as pd
-
 from tqdm import tqdm, trange
 
-
-def clean_verdicts(verdicts):
-    category_map_inverted = {
-        "Correct": ["Accurate", "Correct", "Mostly accurate", "Mostly correct", "Partially correct"],
-        "Misleading": ["Correct but...", "Imprecise", "Lacks context", "Misleading", ""],
-        "Incorrect": ["Flawed reasoning", "Inaccurate", "Incorrect", "Unsupported", "-1.4Very low", "Mostly inaccurate"]
-    }
-    category_map = {}
-    for k, v in category_map_inverted.items():
-        for tag in v:
-            category_map[tag] = k
-
-    out = []
-    for v in verdicts:
-        if v == "":
-            out.append(v)
-        elif "scientific" in v:
-            if "debated" in v: 
-                out.append("Misleading")
-                continue
-            v = float(v.split()[0])
-            if v > 0:
-                out.append("Correct")
-            elif v < 0:
-                out.append("Incorrect")
-            elif v == 0:
-                out.append("Misleading")
-        else:
-            out.append(category_map[v])
-
-    assert len(out) == len(verdicts)
-    return out
 
 url = """https://science.feedback.org/reviews/?_pagination={PAGE}"""
 
@@ -51,26 +16,54 @@ for page in trange(1, 114, desc="fetching links"):
         links.append(link)
 
 topics, claims, verdicts = [], [], []
-for link in tqdm(links, desc="fetching claims/verdicts"):
+for i in trange(len(links), desc="fetching claims/verdicts"):
+    link = links[i]
     response = requests.get(link)
     try: 
         response.raise_for_status()
-        claim_page = BeautifulSoup(response.text, "html.parser")
-        topic = claim_page.find("span", class_="hero__term").text
-        claim_text = claim_page.find("blockquote", class_="reviewed-content__quote").text
-        verdict = claim_page.find("div", class_="col-content").text
-        topics.append(topic)
-        claims.append(claim_text)
-        verdicts.append(verdict)
     except: 
         topics.append("")
         claims.append("")
         verdicts.append("")
+        continue
 
-out = pd.DataFrame(columns=["link", "topic", "claim", "verdict"])
-out["link"] = links
+    page = BeautifulSoup(response.text, "html.parser")  
+    sections = page.find_all("div", class_="sfc-review-reviewed-content")
+    section_claims, section_verdicts = [], []
+    for section in sections:
+        try:
+            topic = page.find("span", class_="hero__term").text
+
+            verdict = section.find_all("div", class_="reviewed-content__row")[0].text.strip()
+            assert verdict.startswith("Verdict:\n")
+            verdict = verdict.split()[-1]; section_verdicts.append(verdict)
+
+            claim = section.find_all("div", class_="reviewed-content__row")[1].text.strip()
+            assert claim.startswith("Claim:\n")
+            claim = [x for x in claim.split("\n")[1:] if x != ""][0]; section_claims.append(claim)
+
+            topics.append(topic)
+            claims.append(claim)
+            verdicts.append(verdict)
+        except:
+            continue
+
+out = pd.DataFrame(columns=["topic", "claim", "verdict"])
 out["topic"] = topics
-out["claim"] = [x.strip() for x in claims]
-out["verdict"] = [x.strip() for x in clean_verdicts(verdicts)]
+out["claim"] = claims
+out["verdict"] = verdicts
 out = out[out["claim"] != ""]
+
+category_map_inverted = {
+    "Incorrect": ["Inaccurate", "Incorrect", "Unsupported", "reasoning", "inaccurate"],
+    "Misleading": ["Misleading", "context", "Imprecise", "but..."],
+    "Correct": ["accurate", "Accurate", "correct", "Correct"]
+}
+category_map = {}
+for k, v in category_map_inverted.items():
+    for tag in v:
+        category_map[tag] = k
+
+out["verdict"] = out["verdict"].apply(lambda x: category_map[x])
+out.drop_duplicates(inplace=True)
 out.to_json("sciencefeedback.jsonl", orient="records", lines=True)
