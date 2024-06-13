5f03684b3855fa3df2c1c73364e82429a4bac8da
Sharan Maiya <29641373+maiush@users.noreply.github.com>
2024-06-13 17:01:31 +0100

    update classifier trainer to output training accuracy

--- src/dev/ipcc_binary_search/train_classifier.py
+++ src/dev/ipcc_binary_search/train_classifier.py
@@ -6,27 +6,29 @@ from PPairS.classify import CompareClassifier
 import torch as t
 import torch.nn.functional as F
 
-from tqdm import tqdm
-
-
-trainer = Trainer(
-    dataset_class = IPCCDataset,
-    dataset_kwargs = {"subset": ["AR6"], "contrast": False, "device": "cpu"},
-    splits = (1., 1.),
-    batch_size = 64
-)
 
 n_epoch = int(sys.argv[1])
 device = sys.argv[2]
+trainer = Trainer(
+    dataset_class = IPCCDataset,
+    dataset_kwargs = {"subset": ["AR6"], "contrast": False, "device": device},
+    splits = (1., 1.),
+    batch_size = 64
+)
 clf = CompareClassifier(device=device)
 opt = t.optim.Adam(clf.parameters(), lr=0.01, weight_decay=0.001)
 for epoch in range(n_epoch):
-    bar = tqdm(trainer.train_loader)
-    for batch, labels in bar:
+    for batch, labels in trainer.train_loader:
         logits = clf(batch)
         loss = F.binary_cross_entropy_with_logits(logits.squeeze(-1), (labels > 0.5).float())
         opt.zero_grad()
         loss.backward()
         opt.step()
-
+    accuracy = []
+    for batch, labels in trainer.train_loader:
+        with t.inference_mode(): logits = clf(batch)
+        P = F.sigmoid(logits)
+        accuracy.append(((P.squeeze(-1) > 0.5) == (labels > 0.5)).float().mean())
+    accuracy = sum(accuracy) / len(accuracy)
+    print(f"epoch {epoch+2} ({round(accuracy.item(), 3)})")
 t.save(clf.state_dict(), f"/gws/nopw/j04/ai4er/users/maiush/PPairS/src/dev/ipcc_binary_search/clf_{n_epoch}.pt")
