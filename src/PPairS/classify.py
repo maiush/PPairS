20f4ca1d4992be2fa5563290e918cdd14308a818
Sharan Maiya <sharan98m@gmail.com>
2024-06-23 23:08:37 +0100

    update classifier to support OVR and multi-class classification

--- src/PPairS/classify.py
+++ src/PPairS/classify.py
@@ -2,22 +2,23 @@ from torch import nn, Tensor
 from jaxtyping import Float
 
 
-class CompareClassifier(nn.Module):
+class ContrastProbe(nn.Module):
 
     def __init__(
         self,
         d_model: int=4096,
+        d_out: int=1,
         device: str="cuda"
     ):
-        super(CompareClassifier, self).__init__()
+        super(ContrastProbe, self).__init__()
         self.d_model = d_model
+        self.d_out = d_out
         self.device = device
 
-        self.map = nn.Linear(in_features=self.d_model, out_features=1, bias=True, device=self.device)
+        self.probe = nn.Linear(in_features=self.d_model, out_features=self.d_out, bias=True, device=self.device)
 
     def forward(
         self,
         x: Float[Tensor, "batch d_model"]
-    ) -> Float[Tensor, "1"]:
-        logits = self.map(x)
-        return logits
+    ) -> Float[Tensor, "d_out"]:
+        return self.probe(x)
