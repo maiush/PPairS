ed742bdc9d41d54cd5a7f45f26859b3a4242fee2
Sharan Maiya <29641373+maiush@users.noreply.github.com>
2024-08-11 20:34:17 +0100

    add utils with list of language models used

---
+++ src/PPairS/utils.py
@@ -0,0 +1,23 @@
+# NOTE: all models are instruction-tuned
+models = {
+    "deepseek-2-236b": "deepseek-ai/DeepSeek-V2-Chat-0628", #  236b (MoE)
+    "mistral-large-123b": "mistralai/Mistral-Large-Instruct-2407", # 123b
+
+    "qwen-2-72b": "Qwen/Qwen2-72B-Instruct", # 72b
+    "llama-3.1-70b": "meta-llama/Meta-Llama-3.1-70B-Instruct", # 70b
+
+    "yi-1.5-34b": "01-ai/Yi-1.5-34B-Chat", # 34b
+    "gemma-2-27b": "google/gemma-2-27b-it", # 27b
+
+    "mistral-nemo-12b": "mistralai/Mistral-Nemo-Instruct-2407", # 12b
+    "phi-3-14b": "microsoft/Phi-3-medium-128k-instruct", # 14b
+
+    "gemma-2-9b-it": "google/gemma-2-9b-it", # 9b
+    "llama-3.1-8b": "meta-llama/Meta-Llama-3.1-8B-Instruct", # 8b
+
+    "phi-3-3.8b": "microsoft/Phi-3-mini-128k-instruct", # 3.8b
+    "gemma-2-2b": "google/gemma-2-2b-it", # 2b
+
+    "mistral-0.1-7b": "mistralai/Mistral-7B-Instruct-v0.1", # 7b,
+    "llama-2-7b": "meta-llama/Llama-2-7b-chat-hf" # 7b
+}
