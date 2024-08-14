35ffc2fc9cc56ecf1d2a15fbb1d078860948e782
Sharan Maiya <29641373+maiush@users.noreply.github.com>
2024-08-14 14:55:07 +0100

    update pipeline to allow for continuation of the prompt as well as regular turn-based conversation

--- src/PPairS/pipeline.py
+++ src/PPairS/pipeline.py
@@ -1,6 +1,6 @@
 import torch as t
 from torch import Tensor
-from transformers import AutoModelForCausalLM, AutoTokenizer, Pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from jaxtyping import Float
 from typing import Union, List, Dict
 
@@ -29,6 +29,8 @@ class PPairSLMPipeline:
     ) -> Union[Float[Tensor, "n_seq n_vocab"], Float[Tensor, "d_model"]]:
         # apply chat template
         prompt = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        # if necessary, allow for continuation instead of QA
+        prompt = self.check_continue(messages, prompt)
         # tokenize
         tks = self.tokenizer(prompt, return_tensors="pt", add_special_tokens=False).to(self.model.device)
         # return logits or residual stream, depending on mode
@@ -48,4 +50,15 @@ class PPairSLMPipeline:
                 out = self.model(tks.input_ids, output_hidden_states=True)
                 return out["hidden_states"][-1].squeeze(0)[-1, :]
 
-
+    def check_continue(
+            self,
+            messages: List[Dict[str, str]],
+            prompt: str
+    ) -> str:
+        '''
+        if we want continuation of the prompt instead of QA, we need to modify the prompt a bit.
+        '''
+        if messages[-1]["role"] != "assistant": return prompt
+        message = messages[-1]["content"]
+        ix = prompt.rindex(message) + len(message)
+        return prompt[:ix]
