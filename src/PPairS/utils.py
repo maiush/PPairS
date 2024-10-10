e5d4e42293a12ad500282b58918c33a227cbfaca
Sharan Maiya <29641373+maiush@users.noreply.github.com>
2024-10-10 15:49:34 +0100

    update utils: using a different suite of models for testing

--- src/PPairS/utils.py
+++ src/PPairS/utils.py
@@ -1,26 +1,28 @@
 # NOTE: all models are instruction-tuned
 models = {
-    "mistral-large-123b": "mistralai/Mistral-Large-Instruct-2407", # 123b
+    # mistral
+    "mistral-large-123b": "mistralai/Mistral-Large-Instruct-2407", 
+    "mistral-small-22b": "mistralai/Mistral-Small-Instruct-2409",
+    "mistral-nemo-12b": "mistralai/Mistral-Nemo-Instruct-2407", 
+     
+    # llama
+    "llama-3.1-70b": "meta-llama/Meta-Llama-3.1-70B-Instruct",
+    "llama-3.1-8b": "meta-llama/Meta-Llama-3.1-8B-Instruct",
 
-    "qwen-2-72b": "Qwen/Qwen2-72B-Instruct", # 72b
-    "llama-3.1-70b": "meta-llama/Meta-Llama-3.1-70B-Instruct", # 70b
+    # gemma
+    "gemma-2-27b": "google/gemma-2-27b-it",
+    "gemma-2-9b": "google/gemma-2-9b-it",
+    "gemma-2-2b": "google/gemma-2-2b-it",
 
-    "yi-1.5-34b": "01-ai/Yi-1.5-34B-Chat", # 34b
-    "gemma-2-27b": "google/gemma-2-27b-it", # 27b
-
-    "mistral-nemo-12b": "mistralai/Mistral-Nemo-Instruct-2407", # 12b
-    "phi-3-14b": "microsoft/Phi-3-medium-128k-instruct", # 14b
-
-    "gemma-2-9b": "google/gemma-2-9b-it", # 9b
-    "llama-3.1-8b": "meta-llama/Meta-Llama-3.1-8B-Instruct", # 8b
-
-    "phi-3-3.8b": "microsoft/Phi-3-mini-128k-instruct", # 3.8b
-    "gemma-2-2b": "google/gemma-2-2b-it", # 2b
-
-    "mistral-0.1-7b": "mistralai/Mistral-7B-Instruct-v0.1", # 7b,
-    "llama-2-7b": "meta-llama/Llama-2-7b-chat-hf" # 7b
+    # qwen
+    "qwen-2.5-72b": "Qwen/Qwen2.5-72B-Instruct",
+    "qwen-2.5-32b": "Qwen/Qwen2.5-32B-Instruct",
+    "qwen-2.5-14b": "Qwen/Qwen2.5-14B-Instruct",
+    "qwen-2.5-7b": "Qwen/Qwen2.5-7B-Instruct",
+    "qwen-2.5-3b": "Qwen/Qwen2.5-3B-Instruct",
+    "qwen-2.5-1.5b": "Qwen/Qwen2.5-1.5B-Instruct",
+    "qwen-2.5-0.5b": "Qwen/Qwen2.5-0.5B-Instruct",
 }
-
 
 # NLG datasets are evaluated on specific aspects
 dataset_aspects = {
